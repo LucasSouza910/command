3319720438fea9cbaf7583fdc643ee74fb756020
Lucas Aguiar <89272961+LucasSouza910@users.noreply.github.com>
2023-10-23 21:01:37 -0300

    Add files via upload

---
+++ commands/src/exemplo/command/example/Demo.java
@@ -0,0 +1,10 @@
+package exemplo.command.example;
+
+import exemplo.command.example.editor.Editor;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
---
+++ commands/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module commands {
+	requires java.desktop;
+}
